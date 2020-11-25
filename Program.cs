e608db29b0a0cbaae88c845961e122dc4612dbb2
Rich Lander <rlander@microsoft.com>
2020-11-24 16:48:06 -0800

    Update program

--- Program.cs
+++ Program.cs
@@ -1,41 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-
-
-Package? lastPackage = null;
-
-List<Package>? packageFamily;
+using System.Text.Json;
 
 string file = "packages.csv";
+Dictionary<string, List<Package>> packages = new();
+IEnumerable<string> linesInFile = File.ReadLines(file);
+linesInFile.GetEnumerator().MoveNext();
 
-bool firstRow = true;
-
-foreach(string line in File.ReadAllLines(file))
+// Get packages
+foreach(string line in linesInFile)
 {
-    if (firstRow)
-    {
-        firstRow = false;
-        continue;
-    }
-
     string[] array = line.Split(',');
     Package package = new(
         array[0].Substring(1,array[0].Length-2),
         array[1].Substring(1,array[1].Length-2),
-        array[2].Substring(1,array[2].Length-2),
         array[3].Substring(1,array[3].Length-2),
-        array[4].Substring(1,array[4].Length-2),
         array[4].Substring(1,array[4].Length-2)
     );
 
-    if (package.Id == lastPackage?.Id)
+    if(!packages.TryGetValue(package.Id, out List<Package>? packageList))
     {
-        
+        packageList = new();
+        packages.Add(package.Id, packageList);
     }
 
+    packageList.Add(package);
 }
 
-record Package(string Id, string Version, string ParsedVersion, string TargetFramework, string PrevTFM, string ChangedTFM);
-record PackageInfo(string Name, PackageTargetFrameworkIgnoreMapping[] Mapping);
+List<PackageIgnoreInfo> packageIgnoreInfo = new();
+PackageSet packageSet = new(packageIgnoreInfo);
+
+// Process packages
+foreach(string name in packages.Keys)
+{
+    List<Package> packageFamily = packages[name];
+    if (packageFamily.Count == 1)
+    {
+        continue;
+    }
+
+    int lastPackage = packageFamily.Count -1;
+    PackageTargetFrameworkIgnoreMapping[] mappings = new PackageTargetFrameworkIgnoreMapping[lastPackage];
+    packageIgnoreInfo.Add(new(name,mappings));
+    Package package = packageFamily[lastPackage];
+    List<string> ignoreVersions = new() {package.Version};
+    for(int i = lastPackage - 1; i >= 0; i--)
+    {
+        package = packageFamily[i];
+        PackageTargetFrameworkIgnoreMapping mapping = new(package.TargetFramework,ignoreVersions.ToArray());
+        mappings[i] = mapping; 
+        ignoreVersions.Add(package.Version);
+    }
+}
+
+// Print data file
+string json = JsonSerializer.Serialize<PackageSet>(packageSet, new()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = true,
+    });
+Console.WriteLine(json);
+
+record Package(string Id, string Version, string TargetFramework, string PrevTFM);
+record PackageIgnoreInfo(string Name, PackageTargetFrameworkIgnoreMapping[] Mapping);
 record PackageTargetFrameworkIgnoreMapping(string TargetFramework, string[] Ignore);
+record PackageSet(List<PackageIgnoreInfo> Packages);
